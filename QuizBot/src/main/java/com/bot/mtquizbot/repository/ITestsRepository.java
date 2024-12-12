ed079752f6b12cbd8c6063f0b8fa20b2e7bf10dc
macbook <macbook@MacBook-Air.local>
2024-12-12 15:43:37 +0500

    implemented method for test result work with db

--- QuizBot/src/main/java/com/bot/mtquizbot/repository/ITestsRepository.java
+++ QuizBot/src/main/java/com/bot/mtquizbot/repository/ITestsRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.bot.mtquizbot.models.Test;
 import com.bot.mtquizbot.models.TestGroup;
+import com.bot.mtquizbot.models.TestResult;
 import com.bot.mtquizbot.models.User;
 
 public interface ITestsRepository {
@@ -15,4 +16,6 @@ public interface ITestsRepository {
     Test getById(String id);
     List<Test> getTestList(TestGroup group);
     void updateTest(Test test);
+    List<TestResult> getTestResultList(User user, Integer limit, Integer offset);
+    void putTestResult(User user, String testId, Integer score);
 }
--- QuizBot/src/main/java/com/bot/mtquizbot/repository/TestsRepository.java
+++ QuizBot/src/main/java/com/bot/mtquizbot/repository/TestsRepository.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Repository;
 
 import com.bot.mtquizbot.models.Test;
 import com.bot.mtquizbot.models.TestGroup;
+import com.bot.mtquizbot.models.TestResult;
 import com.bot.mtquizbot.models.User;
 import com.bot.mtquizbot.models.mapper.TestMapper;
+import com.bot.mtquizbot.models.mapper.TestResultMapper;
 
 @Repository
 public class TestsRepository implements ITestsRepository {
@@ -24,8 +26,15 @@ public class TestsRepository implements ITestsRepository {
 
     private static final String SQL_SELECT_TEST_BY_ID = "" + 
         "SELECT * FROM quizdb.tests WHERE id = ?";
+    
+    private static final String SQL_SELECT_TEST_LIST_RESULTS = "" +
+        "SELECT test_results.user_id, test_results.test_id, test_results.score, " +
+        "test_results.finished_ts FROM quizdb.test_results, quizdb.tests " +
+        "WHERE test_results.user_id = ? AND test_results.test_id = tests.id AND " +
+        "tests.group_id = ? ORDER BY test_results.finished_ts LIMIT ? OFFSET ?";     
 
     protected final static TestMapper TEST_MAPPER = new TestMapper();
+    protected final static TestResultMapper TEST_RESULT_MAPPER = new TestResultMapper();
     protected final JdbcTemplate template;
 
     public TestsRepository(@Qualifier("bot-db") JdbcTemplate template) {
@@ -62,5 +71,13 @@ public class TestsRepository implements ITestsRepository {
             test.getId()
         );
     }
+
+    @Override
+    public List<TestResult> getTestResultList(User user, Integer limit, Integer offset) {
+        return template.query(SQL_SELECT_TEST_LIST_RESULTS, TEST_RESULT_MAPPER,
+                            user.getId(), user.getGroup_id(), limit, offset
+        );
+    }
+
 
 }
